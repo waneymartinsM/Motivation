d6a6e512c9d8fb8f630fa71e51ae2323c73c9da9
Waney Martins <waneymartins2003@hotmail.com>
2023-05-02 19:42:38 -0300

    menus

--- app/src/main/java/com/example/motivation/infra/MotivationConstants.kt
+++ app/src/main/java/com/example/motivation/infra/MotivationConstants.kt
@@ -6,4 +6,9 @@ class MotivationConstants private  constructor() {
         const val USER_NAME = "USER_NAME"
     }
 
+    object FILTER {
+        const val ALL = 1
+        const val HAPPY = 2
+        const val SUNNY = 3
+    }
 }
